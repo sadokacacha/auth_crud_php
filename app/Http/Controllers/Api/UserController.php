c52786162243035dbda38e123e0c89b36f724000
sadokacacha <sadokacacha1998@gmail.com>
2025-04-28 16:50:28 +0100

    adding idk smth in usercontrol

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -12,10 +12,22 @@ class UserController extends Controller
     // GET /api/users
     public function index()
     {
-        return response()->json(User::all());
+        $users = User::with('roles')->get();
+    
+        $users = $users->map(function($user) {
+            return [
+                'id'    => $user->id,
+                'name'  => $user->name,
+                'email' => $user->email,
+                'role'  => $user->roles->pluck('name')->first(), // get first role
+            ];
+        });
+    
+        return response()->json($users);
     }
 
     // POST /api/users
+    
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -36,4 +48,48 @@ class UserController extends Controller
 
         return response()->json($user, 201);
     }
+
+
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+    
+        $data = $request->validate([
+            'name'     => 'required|string|max:255',
+            'email'    => ['required', 'email', Rule::unique('users')->ignore($user->id)],
+            'password' => 'nullable|string|min:6',
+            'role'     => ['required', Rule::in(['admin', 'teacher', 'student'])],
+        ]);
+    
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+    
+        if (!empty($data['password'])) {
+            $user->password = Hash::make($data['password']);
+        }
+    
+        $user->save();
+    
+        // Update roles
+        $user->syncRoles([$data['role']]);
+    
+        return response()->json($user);
+    }
+    
+
+
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+    
+        return response()->json(null, 204);
+    }
+
+
+
+
+
 }
